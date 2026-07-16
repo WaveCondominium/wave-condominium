6aa69100bbbd077a33d1b2b45f57af49226c17cc
Robson Maia <robson.maia1995@gmail.com>
2026-07-16 15:43:01 -0300

    chore(config): libera IP de dev para Server Actions (mobile)

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["localhost:3000", "192.168.18.21:3000"],
+    },
+  },
   // images: {
   //   disableStaticImages: true, // Re-enabled static images
   // },
 };
-
 export default nextConfig;
